b6b6a36dddfde1e645fc46dcf686075de71af041
petxcode <brighterdayscodelab@gmail.com>
2025-01-08 18:21:41 +0100

    pressing

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Peter + React + Pipeline</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          press {count} times
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
